c52ab0d4b16c7eb6dada9ff60b2c95381169fb2d
mjs249 <mjs249@njit.edu>
2023-07-25 17:33:41 -0400

    Dashboard Page

--- public_html/Project/sql/008_create_world_accound.sql
+++ public_html/Project/sql/008_create_world_accound.sql
@@ -1,3 +1,3 @@
-INSERT INTO Accounts (id, account, user_id, balance, account_type, created, modified)
-VALUES (-1, '000000000000', -1, 0, 'world', NOW(), NOW())
-ON DUPLICATE KEY UPDATE account = VALUES(account), user_id = VALUES(user_id), balance = VALUES(balance), account_type = VALUES(account_type), modified = NOW();
+INSERT INTO RM_Accounts (id, account, user_id, balance, created, modified)
+VALUES (-1, '000000000000', -1, 0, NOW(), NOW())
+ON DUPLICATE KEY UPDATE account = VALUES(account), user_id = VALUES(user_id), balance = VALUES(balance), modified = NOW();
--- public_html/Project/sql/009_create_transactions_table.sql
+++ public_html/Project/sql/009_create_transactions_table.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS Transactions (
     expected_total DECIMAL(10, 2) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (account_src) REFERENCES Accounts(id),
-    FOREIGN KEY (account_dest) REFERENCES Accounts(id)
+    FOREIGN KEY (account_src) REFERENCES RM_Accounts(id),
+    FOREIGN KEY (account_dest) REFERENCES RM_Accounts(id)
 );
